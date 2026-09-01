b4f60899221b94a1430b36b1b751f687dc007eaf
William <william.mcm.p@gmail.com>
2026-09-02 07:19:34 +1000

    Add: initial `squares_min_solver` comit

---
+++ squares_min_solver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wednesday September 02 2026
+
+Intened for a first pass of using minimisation of residual 
+squares to find the best fitting spectra to experimentally measured data
+
+@author: william.mcm.p
+"""
